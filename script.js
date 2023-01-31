acf13ad60bbe858790a361a1ac62d9407fff6433
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-01-31 02:12:38 -0300

    volvemos a probar

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("button1").addEventListener("click", function() {
+    document.getElementById("text-container").style.display = "flex";
+  });
+  
+  document.getElementById("close-button").addEventListener("click", function() {
+    document.getElementById("text-container").style.display = "none";
+  });
+  
