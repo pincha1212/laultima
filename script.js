4f1a292ffe4bda814e8dbcb51a07cdfed0638b4c
Ricardo Federico Barzola <fedebarzola7@gmail.com>
2023-01-31 11:37:29 -0300

    Update script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("button1").addEventListener("click", function() {
-    document.getElementById("text-container").style.display = "flex";
-  });
-  
-  document.getElementById("close-button").addEventListener("click", function() {
-    document.getElementById("text-container").style.display = "none";
-  });
-  
